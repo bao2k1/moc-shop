2b71c623f9a39e2bc732c6fd3b084a282f932b76
Bao <mtpquangbao@gmail.com>
2022-06-17 14:09:30 +0700

    updated

---
+++ admin/src/components/Home/ProductsStatistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProductsStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Products statistics</h5>
+          <iframe
+            title="ok"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-music-shop-vhwai/embed/charts?id=62ab806a-ee85-4cec-8e9f-76ba0229bb54&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsStatistics;
---
+++ admin/src/components/Home/SalesStatistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SaleStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Sale statistics</h5>
+          <iframe
+            title="ok"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-music-shop-vhwai/embed/charts?id=62ac1b5a-2a36-48e3-8471-7523abe68af8&maxDataAge=3600&theme=light&autoRefresh=true"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SaleStatistics;
